234cc69284453875cee525659df0dcf3df778422
GrisoFandango <43990276+GrisoFandango@users.noreply.github.com>
2018-12-08 18:23:54 +0000

    Update and rename Vehicles class input to Vehicles class input.py
    adding comment

--- Vehicles class input.py
+++ Vehicles class input.py
@@ -1,14 +1,17 @@
+#creating a class
 class Vehicle():
-    
+    #initialize the class with parameters
     def __init__(self, name, color, brand, engineSize):
         self.name = name
         self.color = color
         self.brand = brand
         self.engineSize = engineSize
-
+    #creating a method to show the parameters values
     def showDetails(self):       
         print(self.name,self.color,self.brand, self.engineSize)
-
+#Create a loop that ask the user to inpunt the different 
+#parameter and then create the object with that parameter
+#and print out the values
 for i in range(1,5):
     name = input ("Enter name of vehicle: ")
     color = input ("Enter color of vehicle: ")
